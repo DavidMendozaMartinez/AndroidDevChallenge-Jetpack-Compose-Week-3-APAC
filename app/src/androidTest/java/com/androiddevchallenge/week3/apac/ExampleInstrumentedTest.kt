f0c3f29efe6c8f94f241bd8ba08510a62c233db6
David Mendoza <david.mendoza.mnez@gmail.com>
2021-05-04 20:57:42 +0200

    Move the MainActivity file to ui package
    Move the MainActivity file to ui package to group all files related to
    the user interface.

--- app/src/androidTest/java/com/androiddevchallenge/week3/apac/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/androiddevchallenge/week3/apac/ExampleInstrumentedTest.kt
@@ -17,6 +17,7 @@ package com.androiddevchallenge.week3.apac
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.androiddevchallenge.week3.apac.ui.MainActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
