dc23d4c7bd99020d0ae3b5dcfcca3baf1c6ef616
David Mendoza <david.mendoza.mnez@gmail.com>
2021-05-06 18:16:26 +0200

    Set the status bar transparent
    Set the decor view to not fit the content view to the insets.
    Add the Accompanist System UI Controller dependency.
    Set all of the system bar colors to be transparent, and use dark icons
    if we're in light theme.

--- app/src/main/java/com/androiddevchallenge/week3/apac/ui/BloomApp.kt
+++ app/src/main/java/com/androiddevchallenge/week3/apac/ui/BloomApp.kt
@@ -15,10 +15,23 @@
  */
 package com.androiddevchallenge.week3.apac.ui
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.androiddevchallenge.week3.apac.ui.theme.BloomTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun BloomApp() {
-    BloomTheme {}
+    BloomTheme {
+        val systemUiController = rememberSystemUiController()
+        val useDarkIcons = MaterialTheme.colors.isLight
+
+        // Update all of the system bar colors to be transparent, and use
+        // dark icons if we're in light theme
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = useDarkIcons
+        )
+    }
 }
