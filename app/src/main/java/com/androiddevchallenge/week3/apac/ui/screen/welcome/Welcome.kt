956d621be6f0d104bf23dfa86e316166d1decaf1
David Mendoza <david.mendoza.mnez@gmail.com>
2021-07-11 16:11:35 +0200

    Add the components to the Welcome screen
    Add the detailed components in the design to the Welcome screen.

--- app/src/main/java/com/androiddevchallenge/week3/apac/ui/screen/welcome/Welcome.kt
+++ app/src/main/java/com/androiddevchallenge/week3/apac/ui/screen/welcome/Welcome.kt
@@ -15,12 +15,105 @@
  */
 package com.androiddevchallenge.week3.apac.ui.screen.welcome
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.androiddevchallenge.week3.apac.R
 import com.androiddevchallenge.week3.apac.ui.theme.BloomTheme
 
 @Composable
 fun Welcome() {
+    Surface(color = MaterialTheme.colors.primary) {
+        Image(
+            painter = painterResource(id = R.drawable.welcome_bg),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            Spacer(modifier = Modifier.height(72.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.welcome_illos),
+                contentDescription = null,
+                modifier = Modifier.padding(start = 88.dp),
+                alignment = Alignment.CenterStart,
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(48.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = stringResource(id = R.string.app_name),
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+
+            Text(
+                text = stringResource(id = R.string.welcome_tagline),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .paddingFromBaseline(top = 32.dp, bottom = 40.dp),
+                color = MaterialTheme.colors.onPrimary,
+                style = MaterialTheme.typography.subtitle1
+            )
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(horizontal = 16.dp),
+                shape = MaterialTheme.shapes.medium,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.secondary
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.welcome_button_create_account),
+                    color = MaterialTheme.colors.onSecondary,
+                    style = MaterialTheme.typography.button
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            TextButton(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(horizontal = 16.dp),
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Text(
+                    text = stringResource(id = R.string.welcome_button_log_in),
+                    color = with(MaterialTheme.colors) { if (isLight) secondary else onPrimary },
+                    style = MaterialTheme.typography.button
+                )
+            }
+        }
+    }
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
