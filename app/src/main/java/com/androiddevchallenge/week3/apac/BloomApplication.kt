f26d77e6bedeebcf36b8fc1433ee8c5722160587
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2021-09-14 13:22:24 +0200

    Create the PexelsSizingInterceptor
    Add the Coil Compose dependency.
    Create the PexelsSizingInterceptor to append query params to Pexels urls
    to request sized images.
    Add the compiler argument '-Xopt-in=kotlin.RequiresOptIn' to use the
    ExperimentalCoilApi class.
    Create the BloomApplication to get the singleton ImageLoader used by
    [rememberImagePainter] to load images in the app.
    Declare the Internet permission in AndroidManifest to load images from
    Pexels.

---
+++ app/src/main/java/com/androiddevchallenge/week3/apac/BloomApplication.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.androiddevchallenge.week3.apac
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.compose.rememberImagePainter
+import com.androiddevchallenge.week3.apac.ui.utils.PexelsSizingInterceptor
+
+@Suppress("unused")
+class BloomApplication : Application(), ImageLoaderFactory {
+
+    /**
+     * Create the singleton [ImageLoader].
+     * This is used by [rememberImagePainter] to load images in the app.
+     */
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .componentRegistry {
+                add(PexelsSizingInterceptor)
+            }
+            .build()
+    }
+}
